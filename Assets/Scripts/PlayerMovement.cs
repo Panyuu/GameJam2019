c1296fb1ba326b9e88d0397a235d7e8bbf12ac56
Tina <tina.kiessling@t-online.de>
2019-11-15 21:55:18 +0100

    Camera Angle Changed

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Stores GameObject player.
+    [SerializeField] GameObject player;
+
+    // KeyCodes are asigned on the model itself.
+    [SerializeField] KeyCode up, down, left, right;
+
+    // Speed is asigned on object itself (rats faster than plaguedoctor).
+    [SerializeField] int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void movePlayer() {
+
+        //if (Input.GetKeyDown.Equals(up)) { 
+        
+        
+        //}
+    
+    }
+}
