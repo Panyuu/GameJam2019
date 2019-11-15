8cab58ae59ca93983ebf60804c06d26d82caeff0
Stefan <stmepp@online.de>
2019-11-16 00:44:08 +0100

    Created Human-/Script and Rat-/Script
    Consume for Rat and Trigger for Human

---
+++ Assets/Scripts/RatEating.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RatEating : MonoBehaviour
+{
+    public GameObject human;
+    public float foodCount;
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Human"))
+        {
+            var human = gameObject.GetComponent<Human>();
+
+            if (human.IsDead)
+            {
+                var consuming = human.GetComponent<Human>();
+                foodCount += consuming.Consume();
+            }
+        }
+
+    }
+}
