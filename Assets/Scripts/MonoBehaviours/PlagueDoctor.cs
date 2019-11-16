e29ef76cfa6c37c9612fecdc62db23b028556d92
Tina <tina.kiessling@t-online.de>
2019-11-16 17:42:39 +0100

    Incense created
    can be picked up and put down.

--- Assets/Scripts/MonoBehaviours/PlagueDoctor.cs
+++ Assets/Scripts/MonoBehaviours/PlagueDoctor.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlagueDoctor : MonoBehaviour {
+
+    public GameObject plagueDoctor;
+
     // Timer until infection reset when health is full.
     int waitForNonInfection;
 
@@ -25,34 +28,38 @@ public class PlagueDoctor : MonoBehaviour {
     // Triggered, while doctor stands next to human.
     private void OnTriggerStay(Collider other) {
 
-        var human = other.GetComponent<Human>();
+        if (other.CompareTag("Human")) {
+            var human = other.GetComponent<Human>();
 
-        // If human is infected and his health is under max. the doctor regenerates the human's health.
-        // When human is dead, he cannot be healed.
-        if (!human.isDead && human.docIsClose && human.health < 100) {
+            // If human is infected and his health is under max. the doctor regenerates the human's health.
+            // When human is dead, he cannot be healed.
+            if (!human.isDead && human.docIsClose && human.health < 100) {
 
-            human.health += 10f * Time.deltaTime;
-        }
+                human.health += 10f * Time.deltaTime;
+            }
 
-        // When health is full one second delay until infection is reversed.
-        else if (human.docIsClose && human.health > 100) {
+            // When health is full one second delay until infection is reversed.
+            else if (human.docIsClose && human.health > 100) {
 
-            //Debug.Log("Hi");
+                //Debug.Log("Hi");
 
-            setWaitForNonInfection(getWaitForNonInfection() + 1);
+                setWaitForNonInfection(getWaitForNonInfection() + 1);
 
-            // Delay.
-            StartCoroutine(resetInfectionMeter(other));
+                // Delay.
+                StartCoroutine(resetInfectionMeter(other));
 
+            }
         }
     }
 
     // Triggered, when doctor leaves human.
     private void OnTriggerExit(Collider other) {
 
-        var human = other.GetComponent<Human>();
+        if (other.CompareTag("Human")) {
+            var human = other.GetComponent<Human>();
 
-        human.docIsClose = false;
+            human.docIsClose = false;
+        }
     }
 
     // For da delay.
